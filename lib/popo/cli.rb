4918deb0a26def05c9c7f5bf789591d82a6573a4
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-06-22 20:05:47 -0500

    Loading...

--- lib/popo/cli.rb
+++ lib/popo/cli.rb
@@ -1,24 +1,22 @@
 class Popo::CLI
 
     def call 
-        begin_cli
+        list_departments
+        officer_id
     end
 
-    def begin_cli
+    def list_departments
         puts "You've come to the right place. Please enter any information you have about the officer. It's okay if you don't have the answer to every question. 
-
+        
         Select a department: 
         1 = Chicago PD
-        2 = Berkeley PD
-        3 = Oakland PD 
-        4 = University of California PD
-        5 = Immigration and Customs Enforcement (ICE)
-        6 = Customs and Border Protection
-        7 = New York City PD
-        8 = Salt Lake City PD
-        9 = Buffalo PD"
+        2 = Immigration and Customs Enforcement (ICE)
+        3 = New York City PD"
 
-        input = gets.chomp()
+        input = gets.strip
+        case input 
+        when "1"
+            puts 
 
         if input == "1" 
             puts 
@@ -26,23 +24,15 @@ class Popo::CLI
             puts 
         elsif input == "3"
             puts 
-        elsif input == "4"
-            puts 
-        elsif input == "5"
-            puts 
-        elsif input == "6"
-            puts 
-        elsif input == "7"
-            puts 
-        elsif input == "8"
-            puts 
-        elsif input == "9"
-            puts 
         else 
             puts "Please enter a number 1-9"
         end
 
 
+    end
+
+    def officer_id
+        puts "Do you remember any part of the officer's last name?"
     end
 
 end

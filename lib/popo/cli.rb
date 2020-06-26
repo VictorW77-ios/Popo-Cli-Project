70e09e597eedd74dbb0efffdc2001d60566f530a
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-06-26 14:45:31 -0500

    Making changes

--- lib/popo/cli.rb
+++ lib/popo/cli.rb
@@ -3,7 +3,7 @@ class Popo::CLI
     def call 
         intro
         user_menu
-        officer_id
+        # officer_id
     end
 
     def intro
@@ -22,33 +22,25 @@ class Popo::CLI
         loop do
             puts "Enter the number of the department you want to search:"
             input = gets.strip.downcase
-            break if input = "exit"
+            break if input == "exit"
             
             if input.to_i > 0
                 the_department = @departments[input.to_i-1]
                 puts "You entered: #{the_department}" #want the array index of the department
-                officer_id
-                break
+                # officer_id
+                # break 
             elsif input == "back"
                 intro
-            elsif input == "exit"
-                puts "Have a good day."
+                break
             else 
                 puts "Please enter 1 or 2. Type 'back' to see the options again. Type 'exit' to exit."
             end
         end
+        puts "Have a good day."
     end
 
     def officer_id
-        # input = gets.strip.downcase
-        # puts "Do you remember any part of the officer's name?"
-        # if input == "yes"
-        #     puts "Please enter whatever you remember:"
-        # elsif input == "no"
-        #     puts "Do you remember any part of the officer's badge number?"
-        # else
-        #     puts "Invalid entry. Please enter any part of the officer's name or type 'no'."
-        # end
+        puts "Do you remember any part of the officer's name?"
     end
 
     # def exit_message 

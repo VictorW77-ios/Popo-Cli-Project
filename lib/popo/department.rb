8f3da9126c969c68aad29b46b35be23887839858
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-06-27 15:05:10 -0500

    Working on attributes

--- lib/popo/department.rb
+++ lib/popo/department.rb
@@ -8,22 +8,24 @@ class Popo::Department
     def self.scrape_dep
         departments = []
 
-        departments << self.chicago_dep
-        departments << self.ny_dep
+        departments << self.scrape_chicago
+        departments << self.scrape_ny
       #I want to return real data 
 
         departments 
     end
 
-    def self.chicago_dep
+    def self.scrape_chicago
         doc = Nokogiri::HTML(open("https://openoversight.com/browse"))
+        
         chi = self.new
         chi.name = doc.css(".btn-group").children[14].text[0..24].strip
         #chi_officers = doc.css("a.btn.btn-lg.btn-primary")[4]["href"]
     end
 
-    def self.ny_dep
+    def self.scrape_ny
         doc = Nokogiri::HTML(open("https://openoversight.com/browse"))
+
         ny = self.new
         ny.name = doc.css(".btn-group h2").last.text.strip
         #ny_officers = doc.css("a.btn.btn-lg.btn-primary")[9]["href"]

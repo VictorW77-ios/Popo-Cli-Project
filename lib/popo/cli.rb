3fcf97e7c707f476bac82c2eb17b83355f1df14e
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-06-27 16:37:09 -0500

    Removed loop, added new method

--- lib/popo/cli.rb
+++ lib/popo/cli.rb
@@ -19,28 +19,27 @@ class Popo::CLI
 
 
     def user_menu 
-        # input = nil
-        # while input != "exit"
-        loop do 
-            puts "\nEnter the number of the department you want to search:"
-            input = gets.strip.downcase
+        puts "\nEnter the number of the department you want to search:"
+        input = gets.strip.to_i 
 
-            if input.to_i == 1 || input.to_i == 2
-                the_department = @departments[input.to_i-1]
-                puts "\nYou entered: #{the_department}"
-                puts "\n"
-                break
-            elsif input == "back"
-                intro
-            elsif input == "exit"
-                puts "\nHave a good day."
-                # break
-                exit
-            else 
-                puts "\nPlease enter 1 or 2. Type 'back' to see the options again. Type 'exit' to exit."
-            end
+        if input.to_i == 1 || input.to_i == 2
+            the_department = @departments[input.to_i-1]
+            puts "\nYou entered: #{the_department}"
+            puts "\n"
+        elsif input == "back"
+            intro
+        elsif input == "exit"
+            puts "\nHave a good day."
+            exit
+        else 
+            puts "\nPlease enter 1 or 2. Type 'back' to see the options again. Type 'exit' to exit."
         end
     end
+
+    def finding_officers 
+
+
+    end 
 
     def officer_id
         puts "Enter the number of the officer you want more information on:"

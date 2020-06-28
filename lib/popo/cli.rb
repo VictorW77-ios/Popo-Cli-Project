4ac47eb0c9349bc671502a200698a50056897d5f
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-06-28 15:47:59 -0500

    Removed in-line comments & refactored if statement

--- lib/popo/cli.rb
+++ lib/popo/cli.rb
@@ -26,7 +26,7 @@ class Popo::CLI
             the_officer = @officers[input.to_i-1]
             puts "You selected: #{the_officer.name}"
             officer_info
-        elsif "back"
+        elsif input == "back"
             general_list
         else 
             puts "\n Please enter a number 1-4. Type 'back' to go back or 'exit' to exit."
@@ -37,8 +37,8 @@ class Popo::CLI
     def officer_info
         puts ""
         puts "        Name: #{officer.name}     "
-        # puts "Race: #{officer.race}     Sex: #{officer.gender}"
-        # puts "Age: #{officer.age}       Badge #: #{officer.badge_num}"
+        puts "Race: #{officer.race}     Sex: #{officer.gender}"
+        puts "Birth Year/Age: #{officer.age}       Badge #: #{officer.badge_num}"
     end 
     
     def exit_message 

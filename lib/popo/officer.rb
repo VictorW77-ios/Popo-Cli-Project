57a0355d5de6dd3520aca6f531d251411a42b88e
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-06-28 14:51:49 -0500

    Add new methods

--- lib/popo/officer.rb
+++ lib/popo/officer.rb
@@ -1,59 +1,52 @@
 class Popo::Officer 
     attr_accessor :name, :race, :gender, :age, :badge_num
 
-    @all = []
+    @@all = []
 
     def initialize(name=nil, race=nil, gender=nil, age=nil, badge_num=nil)
         @name, @race, @gender, @age, @badge_num = name, race, gender, age, badge_num
         @@all << self
     end
 
-    def self.all 
-        @@all
-    end
+    # def self.all 
+    #     @@all
+    # end
 
-    def self.find(info)
-        self.all[i-1]
-    end
+    # def self.find(info)
+    #     self.all[i-1]
+    # end
 
-    # def name 
-    #     @name = 
-    # end 
-
-    # def race 
-    #     @race = 
-    # end 
-
-    # def gender 
-    #     @gender = 
-    # end 
-
-    # def age 
-    #     @age = 
-    # end 
-
-    # def badge_num 
-    #     @badge_num = 
-    # end 
-
-
-    
     def self.officer_scrape
-        chicago_list = []
+        officers = []
         
-        chicago_list << self.chicago_officers
+        officers << self.chicago_officers
 
-        chicago_list
+        officers
         
     end
 
-    def self.chicago_officers
+    def self.officer_one
         doc = Nokogiri::HTML(open("https://openoversight.com/department/1?gender=Not+Sure&min_age=16&rank=Not+Sure&race=Not+Sure&page=1&max_age=100&from_search=False"))
+
+        officer = self.new
+        officer.name = doc.css("div.col-md-6.col-xs-12 a").children[0..3].text.tr("\n"," ").split.join(' ')
+        # officer.race = 
+        # officer.gender = 
+        # officer.age = 
+        # officer.badge_num = 
         # chicago_info = doc.css("div.col-md-6.col-xs-12").children[4..32].text.split.join(' ')
-        # ch_name = doc.css("div.col-md-6.col-xs-12 a").children[0..3].text.tr("\n"," ").split.join(' ')
-        # ch_race = , 
-        # ch_gender = , 
-        # ch_age =   ]
        
+    end
+
+    def self.officer_two
+
+    end 
+
+    def self.officer_three
+
+    end 
+
+    def self.officer_four
+        
     end
 end

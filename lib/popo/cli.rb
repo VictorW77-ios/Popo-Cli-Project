f3694e1ed73ac1db4128f78034fb61b89c7ca02e
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-06-28 14:52:45 -0500

    Updated methods

--- lib/popo/cli.rb
+++ lib/popo/cli.rb
@@ -1,11 +1,11 @@
 class Popo::CLI
 
     def call 
-        intro
-        officers_list
+        general_list
+        choose_officer
     end
 
-    def intro
+    def general_list
         puts "Welcome to the Chicago PD Officer Finder"
         puts "\n"
         @officers = Popo::Officer.chicago_officers
@@ -36,7 +36,7 @@ class Popo::CLI
     #     end
     # end
 
-    def officers_list
+    def choose_officer
         puts "Enter the number of the officer you want more information on:"
         puts "\n"
         input = gets.strip.to_i
@@ -45,9 +45,8 @@ class Popo::CLI
             the_officer = @officers[input.to_i-1]
             puts "You selected: #{the_officer.name}"
             officer_info
-
         elsif "back"
-            intro
+            general_list
         else 
             puts "\n Please enter a number 1-4. Type 'back' to go return to the list or 'exit' to exit."
         end
@@ -55,11 +54,15 @@ class Popo::CLI
     end 
 
     def officer_info
+        puts ""
         puts "        Name: #{officer.name}     "
-        puts "Race: #{officer.race}     Sex: #{officer.gender}"
-        puts "Age: #{officer.age}       Badge #: #{officer.badge_num}"
+        # puts "Race: #{officer.race}     Sex: #{officer.gender}"
+        # puts "Age: #{officer.age}       Badge #: #{officer.badge_num}"
     end 
- 
+    
+    def exit_message 
+        puts "\n Thanks for searching!"
+    end 
         
 
 end

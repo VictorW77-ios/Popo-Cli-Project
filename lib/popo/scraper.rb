1cf4ccf834054113bab797af3775b371c615e015
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-09 12:08:24 -0500

    Refactored get_page method

--- lib/popo/scraper.rb
+++ lib/popo/scraper.rb
@@ -1,51 +1,8 @@
 class Popo::Scraper
 
+    def get_page 
+        Nokogiri::HTML(open("https://openoversight.com/department/1"))
+        binding.pry
+    end 
 
-def self.scrape_one 
-    doc = Nokogiri::HTML(open("https://openoversight.com/officer/11943"))
-    officer = self.new
-    officer.name = doc.css("table.table.table-hover tr td").children[1].text.split.join(' ')
-    officer.race = doc.css("table.table.table-hover tr td").children[9].text
-    officer.gender = doc.css("table.table.table-hover tr td").children[11].text
-    officer.age = doc.css("table.table.table-hover tr td").children[13].text.gsub("None (None)", "Unknown")
-    officer.badge_num = doc.css("table.table.table-hover tbody tr td").children[17].text
-
-    officer
-end
-
-def self.scrape_two
-    doc = Nokogiri::HTML(open("https://openoversight.com/officer/11942"))
-    officer = self.new
-    officer.name = doc.css("table.table.table-hover tr td").children[1].text.split.join(' ')
-    officer.race = doc.css("table.table.table-hover tr td").children[9].text
-    officer.gender = doc.css("table.table.table-hover tr td").children[11].text
-    officer.age = doc.css("table.table.table-hover tr td").children[13].text
-    officer.badge_num = doc.css("table.table.table-hover tr td").children[17].text
-
-    officer
 end 
-
-def self.scrape_three
-    doc = Nokogiri::HTML(open("https://openoversight.com/officer/11944"))
-    officer = self.new
-    officer.name = doc.css("table.table.table-hover tr td").children[1].text.split.join(' ')
-    officer.race = doc.css("table.table.table-hover tr td").children[9].text
-    officer.gender = doc.css("table.table.table-hover tr td").children[11].text
-    officer.age = doc.css("table.table.table-hover tr td").children[13].text
-    officer.badge_num = doc.css("table.table.table-hover tr td").children[17].text
-
-    officer
-end 
-
-def self.scrape_four
-    doc = Nokogiri::HTML(open("https://openoversight.com/officer/11945"))
-    officer = self.new
-    officer.name = doc.css("table.table.table-hover tr td").children[1].text.split.join(' ')
-    officer.race = doc.css("table.table.table-hover tr td").children[9].text
-    officer.gender = doc.css("table.table.table-hover tr td").children[11].text
-    officer.age = doc.css("table.table.table-hover tr td").children[13].text
-    officer.badge_num = doc.css("table.table.table-hover tr td").children[17].text
-
-    officer
-
-end

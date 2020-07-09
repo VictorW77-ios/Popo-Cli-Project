b1b7d5efe74817e35f349c7c2583228b43467c77
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-09 18:27:09 -0500

    Removed comments in choose_officer method

--- lib/popo/cli.rb
+++ lib/popo/cli.rb
@@ -9,7 +9,7 @@ class Popo::CLI
     def general_list
         puts "Welcome to the Chicago PD Officer Finder"
         puts "\n"
-        @officers = Popo::Scraper.officer_scrape
+        @officers = Popo::Scraper.officers_scrape
         
         @officers.each.with_index(1) do |officer, i|
             puts "#{i}. #{officer.name}"
@@ -21,22 +21,22 @@ class Popo::CLI
     def choose_officer
         input = nil
         while input != "exit"
+            puts "\n"
             puts "Enter the number of the officer you want more information on:"
             puts "\n"
             input = gets.strip.downcase
         
-            if input.to_i == 1 || input.to_i == 2 || input.to_i == 3 || input.to_i == 4
+            if input.to_i.between?(1, 20)
                 the_officer = @officers[input.to_i-1]
 
                 puts "        Name: #{the_officer.name}    Badge # : #{the_officer.badge_num} "
-                # puts "Race: #{the_officer.race}            Sex: #{the_officer.gender}"
-                # puts "Birth Year/Age: #{the_officer.age}         
+                puts "Race: #{the_officer.race}            Sex: #{the_officer.gender}"      
                 puts "--------------------------------------------"
                 puts "Type 'back' to search another officer.\n" 
             elsif input == "back"
                 general_list
             else 
-                puts "Please enter a number 1-4. Type 'back' to go back or 'exit' to exit." if input != "exit"
+                puts "Please enter a number 1-20. Type 'back' to go back or 'exit' to exit." if input != "exit"
             end
         end
 

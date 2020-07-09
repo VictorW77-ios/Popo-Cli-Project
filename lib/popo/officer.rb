7bd119cf7c61d4ce65b80195b99210ccf0ae83ec
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-09 11:30:02 -0500

    Removed scraping methods

--- lib/popo/officer.rb
+++ lib/popo/officer.rb
@@ -1,5 +1,27 @@
 class Popo::Officer 
     attr_accessor :name, :race, :gender, :age, :badge_num
+
+    @@all = []
+
+    def initialize(name, race, gender, badge_num, age)
+        @name = name 
+        @race = race
+        @gender = gender 
+        @badge_num = badge_num 
+        @age = age 
+
+        @@all << self 
+    end 
+
+    def name 
+
+        
+    end 
+
+
+    def self.all
+        @@all 
+    end 
     
     def self.chicago 
         self.officer_scrape
@@ -17,52 +39,10 @@ class Popo::Officer
         
     end
 
-    def self.scrape_one 
-        doc = Nokogiri::HTML(open("https://openoversight.com/officer/11943"))
-        officer = self.new
-        officer.name = doc.css("table.table.table-hover tr td").children[1].text.split.join(' ')
-        officer.race = doc.css("table.table.table-hover tr td").children[9].text
-        officer.gender = doc.css("table.table.table-hover tr td").children[11].text
-        officer.age = doc.css("table.table.table-hover tr td").children[13].text.gsub("None (None)", "Unknown")
-        officer.badge_num = doc.css("table.table.table-hover tbody tr td").children[17].text
+    def reading_officers
 
-        officer
-    end
-
-    def self.scrape_two
-        doc = Nokogiri::HTML(open("https://openoversight.com/officer/11942"))
-        officer = self.new
-        officer.name = doc.css("table.table.table-hover tr td").children[1].text.split.join(' ')
-        officer.race = doc.css("table.table.table-hover tr td").children[9].text
-        officer.gender = doc.css("table.table.table-hover tr td").children[11].text
-        officer.age = doc.css("table.table.table-hover tr td").children[13].text
-        officer.badge_num = doc.css("table.table.table-hover tr td").children[17].text
-
-        officer
     end 
 
-    def self.scrape_three
-        doc = Nokogiri::HTML(open("https://openoversight.com/officer/11944"))
-        officer = self.new
-        officer.name = doc.css("table.table.table-hover tr td").children[1].text.split.join(' ')
-        officer.race = doc.css("table.table.table-hover tr td").children[9].text
-        officer.gender = doc.css("table.table.table-hover tr td").children[11].text
-        officer.age = doc.css("table.table.table-hover tr td").children[13].text
-        officer.badge_num = doc.css("table.table.table-hover tr td").children[17].text
-
-        officer
-    end 
-
-    def self.scrape_four
-        doc = Nokogiri::HTML(open("https://openoversight.com/officer/11945"))
-        officer = self.new
-        officer.name = doc.css("table.table.table-hover tr td").children[1].text.split.join(' ')
-        officer.race = doc.css("table.table.table-hover tr td").children[9].text
-        officer.gender = doc.css("table.table.table-hover tr td").children[11].text
-        officer.age = doc.css("table.table.table-hover tr td").children[13].text
-        officer.badge_num = doc.css("table.table.table-hover tr td").children[17].text
-
-        officer
-
-    end
+    # make separate file for scraping 
+   
 end

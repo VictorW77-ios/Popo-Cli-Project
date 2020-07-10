d3c5d3aa28cfb7b2206c69382bd82fa17a806c7e
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-10 11:12:14 -0500

    Changed "popo.rb" to "environment.rb" for clarity

--- lib/popo.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-
-
-require_relative "./popo/version"
-require_relative './popo/cli'
-require_relative './popo/officer'
-require_relative './popo/scraper'

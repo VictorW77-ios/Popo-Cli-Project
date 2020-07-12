055513d1c8c30ac0f414265a02577abc606d7c59
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-12 15:19:31 -0500

    Refactored #general_list to account for changed Officer class, added general design/colorize stuff

--- lib/popo/cli.rb
+++ lib/popo/cli.rb
@@ -1,49 +1,44 @@
+# The CLI classes houses the code responsible for the user interface
+
 class Popo::CLI
 
     def call 
+        Popo::Scraper.make_officers # routes to Scraper class #make_officers method
         general_list
         choose_officer
         exit_message
     end
 
     def general_list
-        puts "Welcome to the Chicago PD Officer Finder"
+        puts "--------".colorize(:red) + "Welcome to the Chicago PD Officer Finder" + "--------".colorize(:red)
         puts "\n"
-        @officers = Popo::Scraper.get_page
-        
-        @officers.each.with_index(1) do |officer, i|
-            puts "#{i}. #{officer.name}"
-        end
-        
+        Popo::Officer.list_officers # routes to Officer class #list_officers method
+        puts "\n"
+        puts "Enter the number of the officer you want more information on:"
+        puts "\n"
     end
 
 
     def choose_officer
-        input = nil
+        input = ""
         while input != "exit"
-            puts "\n"
-            puts "Enter the number of the officer you want more information on:"
-            puts "\n"
             input = gets.strip.downcase
-        
-            if input.to_i.between?(1, 20)
-                the_officer = @officers[input.to_i-1]
-
-                puts "        Name: #{the_officer.name}    Badge # : #{the_officer.badge_num} "
-                puts "Race: #{the_officer.race}            Sex: #{the_officer.gender}"      
-                puts "--------------------------------------------"
-                puts "Type 'back' to search another officer.\n" 
+            if input.to_i.between?(1, Popo::Officer.all.length) # better range instead of "1, 20" - also makes it easier to refactor later on
+                Popo::Officer.list_officer_info(input)
+                puts "\n"
+                puts "Enter another number to see the coinciding officer"
+                puts "Type" + " 'back'".colorize(:red) + " to go to the main menu"
             elsif input == "back"
                 general_list
             else 
-                puts "Please enter a number 1-20. Type 'back' to go back or 'exit' to exit." if input != "exit"
+                puts "Please enter a number 1-20. Type 'back' to go back or 'exit' to exit.".colorize(:blue) if input != "exit"
             end
         end
 
     end 
 
     def exit_message 
-        puts "Stay informed!"
+        puts "-----" + " Stay informed! ".colorize(:red) + "-----"
     end 
         
 

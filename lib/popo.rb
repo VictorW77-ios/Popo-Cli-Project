0a9c05597969f2cc888252ea81116e20a700fad9
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-12 13:00:50 -0500

    Created Error class in Popo module

--- lib/popo.rb
+++ lib/popo.rb
@@ -1,10 +1,14 @@
 require 'open-uri'
 require 'nokogiri'
-require 'colorize'
 require 'pry'
-
+require 'colorize'
 
 require_relative "./popo/version"
 require_relative './popo/cli'
 require_relative './popo/officer'
 require_relative './popo/scraper'
+
+
+module Popo 
+    class Error < StandardError; end 
+end

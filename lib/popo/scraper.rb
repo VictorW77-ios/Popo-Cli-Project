9709d2f865d45df755aba0643c6a2bcd44666f98
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-12 15:18:15 -0500

    Deleted #initialize, added #get_officer_info, refactored #make_officers

--- lib/popo/scraper.rb
+++ lib/popo/scraper.rb
@@ -1,48 +1,24 @@
-require_relative 'officer'
+# The Scraper class' job is to scrape info from openoversight.com
 
 class Popo::Scraper
-    attr_accessor :name, :race, :gender, :badge_num
-
-    def initialize
-       @name = name 
-       @race = race 
-       @gender = gender 
-       @badge_num = badge_num
-    end 
-    # defining an initialize method here chains this 
-    # to the initialize method in the Officer class. 
-    # This " chained method " assigns values to @name, @race, etc..
-    # NOTE: find a way for Scraper class to inherit Officer attrributes
-    # amd assign those attr's values in my scrape_openoversight method
+    
     def self.get_page 
-        self.officers_scrape
+        Nokogiri::HTML(open("https://openoversight.com/department/1"))
     end 
 
-    def self.officers_scrape 
-        officers = [] 
-        # binding.pry
-
-        officers << self.scrape_officer_info
-
-        officers
+    def self.get_officer_info
+        self.get_page.css(".list-group-item")
     end 
 
-    def self.scrape_officer_info
-        doc = Nokogiri::HTML(open("https://openoversight.com/department/1"))
-        officer = self.new
-        
-        officer.name = doc.css("h2 a").map {|n| n.text.strip.tr("\n", " ") }
-        officer.race = doc.css(".list-group-item > div.row > div:last-child > .row > div:first-child > dl > dd:last-child").text.split(/([[:upper:]][[:lower:]]*)/).join(" ").split(",")
-        officer.gender = doc.css(".list-group-item > div.row > div > .row > div:last-child > dl > dd").text.delete("0").scan /\w/
-        officer.badge_num = doc.css("h2 small").map {|el| el.text.strip}
+    def self.make_officers # creates new instances of an Officer // defines it's attributes
+        self.get_officer_info.each { |element| 
+            name = element.css("h2 a").map {|n| n.text.strip.tr("\n", " ") } # returns a string within an array -- looks cool
+            race = element.css(".list-group-item > div.row > div:last-child > .row > div:first-child > dl > dd:last-child").map { |r| r.text.strip.split(/([[:upper:]][[:lower:]]*)/).join("") }
+            gender = element.css(".list-group-item > div.row > div > .row > div:last-child > dl > dd").map { |g| g.text.delete("0").scan /\w/ }.reject(&:empty?)
+            badge_num = element.css("h2 small").map {|bn| bn.text.strip}
 
-        officer
+            Popo::Officer.new(name, race, gender, badge_num) # creates a new instance of an Officer 
+        }
     end 
 
-    # def create_officers
-    #     officers_scrape.each do |officer| # iterated over each officer
-    #         Popo::Officer.new_officer_by_index(officer)
-    #     end
-    # end
-            
 end 
